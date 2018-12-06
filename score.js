2b56bc67ef62c8b8791a8bf957546541cb8171f8
Christian <chansen302@gmail.com>
2018-12-05 21:04:49 -0800

    Includes function which stores user name and score, but only if the score is higher than the previous entry.  Still need to test initial condition for when no score is stored.

--- score.js
+++ score.js
@@ -12,18 +12,16 @@ authorize = firebase.auth(); // To authorize user
 database = firebase.database(); // To access database
 
 // Global Variables
-var scoreKey;
+var userID; // Just in case
 
-// Sign in a user from the browser to allow read/write permissions
+// Sign in a user from the browser to allow read/write permissions (and log errors)
 authorize.signInAnonymously().catch(function(error) {
     console.log("Error Code: " + error.code);
 });
 
 // Actions to do on sign-in
-firebase.auth().onAuthStateChanged(function() {
-    scoreKey = database.ref().child('Scores').push({
-        score: ["", 0]
-    }).key;
+firebase.auth().onAuthStateChanged(function(user) {
+    userID = user.uid;
 })
 
 // This function will pull the scores from the database and sort them on the client
@@ -31,10 +29,20 @@ function clientSort() {
 
 }
 
+
+
 // Function to store user score and name onto database
-/* function storeScore(username, score) {
-    database.ref('Scores/').once('')
-    if () {
-        database.ref('Scores/').push()
-    }
-} */
+// Must be able to tell if the 
+function storeScore(username, score) {
+    database.ref('Scores/' + userID).once('value')
+    .then(function(scoreData) {
+        console.log(scoreData.val());
+        console.log(scoreData.val().score);
+        if (isNaN(scoreData.val().score) || score > scoreData.val().score) {
+            database.ref('Scores/' + userID).set({
+            name: username,
+            score: score
+            })
+        }
+    })
+}
