8d12bc27f2f0504e4559da21cf67d38ebcbefaca
Christian <chansen302@gmail.com>
2018-12-05 22:52:41 -0800

    Had to remove conditionals to test if score was lower because checking if a directory even exists consistently is apparently a challenge for Firebase

--- score.js
+++ score.js
@@ -13,6 +13,7 @@ database = firebase.database(); // To access database
 
 // Global Variables
 var userID; // Just in case
+var highScore; // Because Firebase can't be trusted
 
 // Sign in a user from the browser to allow read/write permissions (and log errors)
 authorize.signInAnonymously().catch(function(error) {
@@ -24,25 +25,20 @@ firebase.auth().onAuthStateChanged(function(user) {
     userID = user.uid;
 })
 
+
 // This function will pull the scores from the database and sort them on the client
 function clientSort() {
 
 }
 
 
-
 // Function to store user score and name onto database
-// Must be able to tell if the 
+// Must be able to tell if the user has already entered data
+// and prevent lower scores from overwriting higher scores.
+// MAYBE LATER, RIGHT NOW, FIREBASE REFUSES TO WORK WELL ENOUGH TO RELIABLY RUN SIMPLE CONDITIONALS
 function storeScore(username, score) {
-    database.ref('Scores/' + userID).once('value')
-    .then(function(scoreData) {
-        console.log(scoreData.val());
-        console.log(scoreData.val().score);
-        if (isNaN(scoreData.val().score) || score > scoreData.val().score) {
-            database.ref('Scores/' + userID).set({
-            name: username,
-            score: score
-            })
-        }
+    database.ref("Score/" + userID).set({
+        name: username,
+        score: score
     })
 }
