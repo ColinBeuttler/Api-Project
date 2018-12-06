b61813f3996dab7f1144dcbd3ffee9e9864bb18c
Colin <beutt102@mail.chapman.edu>
2018-12-06 13:13:59 -0800

    updated dropdown

--- action.js
+++ action.js
@@ -1,17 +1,43 @@
 
-object("#dropdown-item").onclick = function(){
-    var output = document.getElementById('characterId')
-    output.innerHTML="img"+"CharaterDataContainer";
+// object("#dropdown-item").onclick = function(){
+//     var output = document.getElementById('characterId')
+//     output.innerHTML="img"+"CharaterDataContainer";
+// };
+
+// getCharacters= function(){
+//     access marvel api
+//     retrieve the questions within the list for each character
+    
+
+var PRIV_KEY = "509ef6241a118527ba0cc4159410064e07950f14";
+var PUBLIC_KEY = "c75a405d177bd0e520a0611c6f73e954";
+
+function getMarvelResponse() {
+    var ts = new Date().getTime();
+    var hash = CryptoJS.MD5(ts + PRIV_KEY + PUBLIC_KEY).toString();
 };
 
-//getCharacters= function(){
-    //access marvel api
-    //retrieve the questions within the list for each character
-    //
-//};
+// var characterName 
+// $getJSON (getCharacters)
 
-let dropdown = document.querySelector('.dropdown');
-       dropdown.addEventListener('click', function (event) {
-           event.stopPropagation();
-           dropdown.classList.toggle('is-active');
-       });
+// url= ("https://gateway.marvel.com:443/v1/public/characters");
+
+
+
+        
+        
+
+     
+     
+
+
+
+// let dropdown = document.querySelector('.dropdown');
+//        dropdown.addEventListener('click', function (event) {
+//            event.stopPropagation();
+//            dropdown.classList.toggle('is-active');
+//        });
+
+    //    $( "#Spider-Man" ).click(function() {
+    //     console.log ("Spiderman" );
+    //   });
