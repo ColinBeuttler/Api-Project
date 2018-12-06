8e66be4fd75bbe2b4993072d6dcc27c15cfcd05f
Christian <chansen302@gmail.com>
2018-12-06 13:31:20 -0800

    Have good results using Firebase methods for read and write, with any future changes likely being preferential or cosmetic.  Current commit includes basic input forms to test functions with a submit event which calls these functions based on conditions being tracked by the script.

--- score.js
+++ score.js
@@ -12,8 +12,8 @@ authorize = firebase.auth(); // To authorize user
 database = firebase.database(); // To access database
 
 // Global Variables
-var userID; // Just in case
-var highScore; // Because Firebase can't be trusted
+var userID; // For storing the user's ID
+var scoreData; // Global variable to store score data from Firebase in realtime
 
 // Sign in a user from the browser to allow read/write permissions (and log errors)
 authorize.signInAnonymously().catch(function(error) {
@@ -23,22 +23,50 @@ authorize.signInAnonymously().catch(function(error) {
 // Actions to do on sign-in
 firebase.auth().onAuthStateChanged(function(user) {
     userID = user.uid;
+    // Get the state of the user's score at all times (but only after the userID has been obtained)
+    database.ref('Scores/' + userID).on('value', function(currentScoreData) {
+        console.log(currentScoreData.val());
+        scoreData = currentScoreData.val();
+    }, function(error) {
+        console.log("Error code: " + error.code);
+    })
 })
-
 
 // This function will pull the scores from the database and sort them on the client
 function clientSort() {
 
 }
 
+// Function for updating the user's score data without using overwrites
+function storeNewScore(score) { 
+    if (scoreData.score < score) {
+        database.ref("Scores/" + userID).update({
+            score: score
+        })
+    }
+}
 
-// Function to store user score and name onto database
-// Must be able to tell if the user has already entered data
-// and prevent lower scores from overwriting higher scores.
-// MAYBE LATER, RIGHT NOW, FIREBASE REFUSES TO WORK WELL ENOUGH TO RELIABLY RUN SIMPLE CONDITIONALS
-function storeScore(username, score) {
-    database.ref("Score/" + userID).set({
+// Function for storing and setting the initial score
+function storeInitialScore(username, score) {
+    database.ref("Scores/" + userID).set({
         name: username,
         score: score
     })
+    userSet = true; // Confirm that the user has a Firebase Score
 }
+
+// User input event functions (for testing purposes)
+$("#submit").click(function(event) {
+    event.preventDefault(); // Keeps submit button from refreshing page
+    var name = $("#name").val().trim();
+    var score = parseInt($("#score").val().trim());
+    if (isNaN(score) || name == null) {
+        return;
+    }
+    else if (scoreData === null) {
+        storeInitialScore(name, score);
+    }
+    else {
+        storeNewScore(score);
+    }
+})
