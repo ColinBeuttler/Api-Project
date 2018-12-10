9daa28fe67e63719b63b8411fb9812b21ebbba03
Colin <beutt102@mail.chapman.edu>
2018-12-10 09:29:28 -0800

    updated Java

--- java.js
+++ java.js
@@ -6,86 +6,87 @@ var config = {
     projectId: "project-1-marvelous",
     storageBucket: "project-1-marvelous.appspot.com",
     messagingSenderId: "946464641688"
-    };
-    firebase.initializeApp(config);
-    authorize = firebase.auth(); // To authorize user
-    database = firebase.database(); // To access database
-    
-    // Global Variables
-    var userID; // For storing the user's ID
-    var scoreData; // Global variable to store score data from Firebase in realtime
-    var scoreOrderedList = []; // Contains the ordered list of scores and names
-    
-    // Sign in a user from the browser to allow read/write permissions (and log errors)
-    authorize.signInAnonymously().catch(function(error) {
-        console.log("Error Code: " + error.code);
-    });
-    
-    // Actions to do on sign-in
-    firebase.auth().onAuthStateChanged(function(user) {
-        userID = user.uid;
-        // Get the state of the user's score at all times (but only after the userID has been obtained)
-        database.ref('Scores/' + userID).on('value', function(currentScoreData) {
-            console.log(currentScoreData.val());
-            scoreData = currentScoreData.val();
-        }, function(error) {
-            console.log("Error code: " + error.code);
-        })
-    });
-    
-    // This function will pull the scores from the database and sort into an array on the client
-    database.ref('Scores').orderByChild('score').on('value', function(scoreOrder) {
-        var scoreBoardTemp = []; // Use this instead of pushing directly to global
-        // Loops through the score object in order of the ascending score values of the children
-        scoreOrder.forEach(function(child) {
-            // console.log(child.val());
-            // console.log(child.val().name);
-            // console.log(child.val().score);
-            var scoreInstance = [child.val().name, child.val().score]; // Stores an array with both name and score
-            scoreBoardTemp.push(scoreInstance); // Pushes above array onto a separate one for a 2D array
-            })
-            scoreOrderedList = scoreBoardTemp.reverse(); // Preserves current sorted score array without pushing on duplicates
-            scoreOrderedList.length = 10; // Cut out extra values after the 12th score (scoreboard is top 10)
-        }, function(error) {
-            console.log("Error code: " + error.code);
-        });
-    
-    // This function does not need to be used and can be easily replaced, but is for the convenience of printing the score array.
-    // Replace with preferred function
-    function listScores() {
-        for (i=0; i<scoreOrderedList.length; i++) {
-            $("#scoreBoard").append('<h2>' + 'Name: ' + scoreOrderedList[i][0] + '\t' + 'Score: ' + scoreOrderedList[i][1]);
-        }
-    };
-    
-    // Function for updating the user's score data without using overwrites
-    function storeNewScore(score) { 
-        if (scoreData.score < score) {
-            database.ref("Scores/" + userID).update({
-                score: score
-            })
-        }
-    };
-    
-    // Function for storing and setting the initial score
-    function storeInitialScore(username, score) {
-        database.ref("Scores/" + userID).set({
-            name: username,
+};
+firebase.initializeApp(config);
+authorize = firebase.auth(); // To authorize user
+database = firebase.database(); // To access database
+
+// Global Variables
+var userID; // For storing the user's ID
+var scoreData; // Global variable to store score data from Firebase in realtime
+var scoreOrderedList = []; // Contains the ordered list of scores and names
+
+// Sign in a user from the browser to allow read/write permissions (and log errors)
+authorize.signInAnonymously().catch(function (error) {
+    console.log("Error Code: " + error.code);
+});
+
+// Actions to do on sign-in
+firebase.auth().onAuthStateChanged(function (user) {
+    userID = user.uid;
+    // Get the state of the user's score at all times (but only after the userID has been obtained)
+    database.ref('Scores/' + userID).on('value', function (currentScoreData) {
+        console.log(currentScoreData.val());
+        scoreData = currentScoreData.val();
+    }, function (error) {
+        console.log("Error code: " + error.code);
+    })
+});
+
+// This function will pull the scores from the database and sort into an array on the client
+database.ref('Scores').orderByChild('score').on('value', function (scoreOrder) {
+    var scoreBoardTemp = []; // Use this instead of pushing directly to global
+    // Loops through the score object in order of the ascending score values of the children
+    scoreOrder.forEach(function (child) {
+        // console.log(child.val());
+        // console.log(child.val().name);
+        // console.log(child.val().score);
+        var scoreInstance = [child.val().name, child.val().score]; // Stores an array with both name and score
+        scoreBoardTemp.push(scoreInstance); // Pushes above array onto a separate one for a 2D array
+    })
+    scoreOrderedList = scoreBoardTemp.reverse(); // Preserves current sorted score array without pushing on duplicates
+    scoreOrderedList.length = 10; // Cut out extra values after the 12th score (scoreboard is top 10)
+}, function (error) {
+    console.log("Error code: " + error.code);
+});
+
+// This function does not need to be used and can be easily replaced, but is for the convenience of printing the score array.
+// Replace with preferred function
+function listScores() {
+    for (i = 0; i < scoreOrderedList.length; i++) {
+        $("#scoreBoard").append('<h2>' + 'Name: ' + scoreOrderedList[i][0] + '\t' + 'Score: ' + scoreOrderedList[i][1]);
+    }
+};
+
+// Function for updating the user's score data without using overwrites
+function storeNewScore(score) {
+    if (scoreData.score < score) {
+        database.ref("Scores/" + userID).update({
             score: score
         })
-        userSet = true; // Confirm that the user has a Firebase Score
-    };
-    
-    
-    
+    }
+};
+
+// Function for storing and setting the initial score
+function storeInitialScore(username, score) {
+    database.ref("Scores/" + userID).set({
+        name: username,
+        score: score
+    })
+    userSet = true; // Confirm that the user has a Firebase Score
+};
+
+
+
 $(document).ready(function () {
-   
+
     // Global variables
     var card = $("#trivia");
     var timerNumber = 30;
     var timer; //used for Set Interval
     var character = "";
     var questions = 5;
+    var firstcharClick = false;
 
     //Array of 5 Marvel Character Objects with 5 questions and 3 choices
 
@@ -257,7 +258,7 @@ $(document).ready(function () {
     ];
 
     var game = {
-        character: Thor,
+        character: "",
         questionNum: 0,
         timerCnt: timerNumber, //set to 30 
         rightAnswers: 0,
@@ -324,16 +325,19 @@ $(document).ready(function () {
             game.questionNum = 0; //reset question # counter back to 0
 
             //clear timer area of screen
-            $("#timer-body").empty();          
+            $("#timer-body").empty();
 
             card.html("Game over. Your results: <br>");
 
             //$("#counter-number").text(game.timerCnt);
 
             card.append("Correct Answers: " + game.rightAnswers + "<br>");
-            card.append("Incorrect Answers: " + game.wrongAnswers + "<br>" );
+            card.append("Incorrect Answers: " + game.wrongAnswers + "<br>");
             // Call ScoreboardDisplay(UserName, game.rightAnswers); 
-            // reset game
+
+            // reset game, character back to null & set 1st time character selected back to false
+            character = "";
+            firstcharClick = false;
         },
 
         clicked: function (e) { //Check for right answer
@@ -390,46 +394,115 @@ $(document).ready(function () {
     });
 
     $(document).on("click", "#thor", function () {
-        console.log("Thor Questions");
+        if (character === "") {
 
-        $("#timer-body").prepend("Time Remaining: <span id='counter-number'>30</span> Seconds");
+            $("#timer-body").prepend("Time Remaining: <span id='counter-number'>30</span> Seconds");
 
-        character = Thor;
-        game.getnextQuestion(character);
+            character = Thor;
+
+            firstcharClick = true; //set the fact that you already clicked on a character
+
+            game.getnextQuestion(character);
+        }
+        else if (firstcharClick) {      //Check if already clicked on Thor, if not it's another character
+            if (character === Thor) {
+
+                $("#timer-body").prepend("Time Remaining: <span id='counter-number'>30</span> Seconds");
+
+                game.getnextQuestion(character);
+            }
+        }
+        // otherwise clicked on another character without game being over
     });
 
     $(document).on("click", "#spiderman", function () {
-        console.log("Spider Man Questions");
+        if (character === "") {
 
-        $("#timer-body").prepend("Time Remaining: <span id='counter-number'>30</span> Seconds");
+            $("#timer-body").prepend("Time Remaining: <span id='counter-number'>30</span> Seconds");
 
-        character = SpiderMan;
-        console.log("Character=" + character);
-        game.getnextQuestion(character);
+            character = SpiderMan;
+
+            firstcharClick = true;
+
+            game.getnextQuestion(character);
+        }
+        else if (firstcharClick) {
+            //Check if already clicked on SpiderMan, if not it's another character
+            if (character === SpiderMan) {
+
+                $("#timer-body").prepend("Time Remaining: <span id='counter-number'>30</span> Seconds");
+
+                game.getnextQuestion(character);
+            }
+        }
     });
 
     $(document).on("click", "#ironman", function () {
-        $("#timer-body").prepend("Time Remaining: <span id='counter-number'>30</span> Seconds");
-        console.log("Iron Man Questions");
+        if (character === "") {
 
-        character = IronMan;
-        game.getnextQuestion(character);
+            $("#timer-body").prepend("Time Remaining: <span id='counter-number'>30</span> Seconds");
+
+            character = IronMan;
+
+            firstcharClick = true; //set the fact that you already clicked on a character
+
+            game.getnextQuestion(character);
+        }
+        else if (firstcharClick) {
+            //Check if already clicked on IronMan, if not it's another character
+            if (character === IronMan) {
+
+                $("#timer-body").prepend("Time Remaining: <span id='counter-number'>30</span> Seconds");
+
+                game.getnextQuestion(character);
+            }
+        }
     });
 
     $(document).on("click", "#avengers", function () {
-        $("#timer-body").prepend("Time Remaining: <span id='counter-number'>30</span> Seconds");
-        console.log("Avenger Questions");
+        if (character === "") {
 
-        character = Avengers;
-        game.getnextQuestion(character);
+            $("#timer-body").prepend("Time Remaining: <span id='counter-number'>30</span> Seconds");
+
+            character = Avengers;
+
+            firstcharClick = true; //set the fact that you already clicked on a character
+
+            game.getnextQuestion(character);
+        }
+        else if (firstcharClick) {
+            //Check if already clicked on IronMan, if not it's another character
+            if (character === Avengers) {
+
+                $("#timer-body").prepend("Time Remaining: <span id='counter-number'>30</span> Seconds");
+
+                game.getnextQuestion(character);
+            }
+        }
     });
 
     $(document).on("click", "#captainamerica", function () {
-        $("#timer-body").prepend("Time Remaining: <span id='counter-number'>30</span> Seconds");
-        console.log("Captain America Questions");
+        if (character === "") {
 
-        character = CaptainAmerica;
-        game.getnextQuestion(character);
+            $("#timer-body").prepend("Time Remaining: <span id='counter-number'>30</span> Seconds");
+
+            character = CaptainAmerica;
+
+            firstcharClick = true; //set the fact that you already clicked on a character
+
+            game.getnextQuestion(character);
+        }
+
+        else if (firstcharClick) {
+            //Check if already clicked on IronMan, if not it's another character
+            if (character === Avengers) {
+
+                $("#timer-body").prepend("Time Remaining: <span id='counter-number'>30</span> Seconds");
+
+                game.getnextQuestion(character);
+            }
+        }
     });
+
 
 });
